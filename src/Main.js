86f197cbac94c324e74547cc7cbdfab2407794b1
roopekal <roope.kallio@myy.haaga-helia.fi>
2022-11-22 12:48:33 +0200

    redux lisätty projektiin

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ControlPanel from "./container/ControlPanel/ControlPanel.jsx";
 import App from "./App.js";
 
 const Main = () => {
    return (
-      <Routes>
-         <Route exact path="/" element={<App />}></Route>
-         <Route exact path="/cpanel" element={<ControlPanel />}></Route>
-      </Routes>
+      <BrowserRouter>
+         <Routes>
+            <Route exact path="/" element={<App />}></Route>
+            <Route exact path="/cpanel" element={<ControlPanel />}></Route>
+         </Routes>
+      </BrowserRouter>
    );
 };
 
--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const store = configureStore({
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <BrowserRouter>
+   <Provider store={store}>
       <Main />
-   </BrowserRouter>
+   </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
